4247697717093dfd13f9b2ce4c950a2899b871bb
Carlos Lucas <clucas@hotelbeds.com>
2019-11-09 12:12:12 +0100

    Map and player movement basics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float localSpeed = speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * localSpeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * localSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * localSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * localSpeed);
+        }
+    }
+}
