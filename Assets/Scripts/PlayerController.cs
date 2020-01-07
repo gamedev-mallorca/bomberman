4c399cb7a8179a53fe2258d5c9261bb6f1933caf
Carlos Lucas <clucas@hotelbeds.com>
2020-01-07 21:55:46 +0100

    center bomb spawn on cell

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public BombTimer bomb;
-    public int speed,bombPocket;
+    public BombTimer bombPrefab;
+    public int speed, bombPocket;
+
+    private GameObject map;
 
     // Start is called before the first frame update
     void Start()
-    {   
+    {
+        map = GameObject.FindGameObjectWithTag("map");
     }
 
     // Update is called once per frame
@@ -18,11 +22,8 @@ public class PlayerController : MonoBehaviour
     {
         float localSpeed = speed * Time.deltaTime;
 
-        if(Input.GetKey(KeyCode.Space) && bombPocket>0){
-            bombPocket--;
-            BombTimer clone=Instantiate<BombTimer>(bomb,transform.position,transform.rotation);
-            clone.player=this;
-            clone.bomb=clone;
+        if(Input.GetKey(KeyCode.Space)){
+            spawnBomb();
         }
 
         if (Input.GetKey(KeyCode.W))
@@ -41,5 +42,24 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector3.right * localSpeed);
         }
+    }
+
+    public void spawnBomb()
+    {
+        if (bombPocket > 0)
+        {
+            bombPocket--;
+            Vector3 spawnPosition = getBombSpawnPosition();
+            BombTimer bomb = Instantiate<BombTimer>(bombPrefab, spawnPosition, Quaternion.identity);
+            bomb.player = this;
+            bomb.bomb = bomb;
+        }
+    }
+
+    private Vector3 getBombSpawnPosition()
+    {
+        Grid grid = map.GetComponent<Grid>();
+        Vector3Int cellSize = grid.WorldToCell(transform.position);
+        return grid.GetCellCenterWorld(cellSize);
     }
 }
