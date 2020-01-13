d6b900b253a23a1b7729b33e588a1584b70ecbf2
Carlos Lucas <clucas@hotelbeds.com>
2020-01-14 00:55:11 +0100

    explosion animation

--- Assets/Scripts/BombTimer.cs
+++ Assets/Scripts/BombTimer.cs
@@ -8,6 +8,7 @@ public class BombTimer : MonoBehaviour
     public double timer;
     public PlayerController player;
     public int explosionRange;
+    public GameObject explosionEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -29,16 +30,21 @@ public class BombTimer : MonoBehaviour
     private void explode()
     {
         Grid grid = GameObject.FindGameObjectWithTag("map").GetComponent<Grid>();
-        doExplosionRay(Vector2.up, grid.cellSize.y * explosionRange);
-        doExplosionRay(Vector2.down, grid.cellSize.y * explosionRange);
-        doExplosionRay(Vector2.left, grid.cellSize.x * explosionRange);
-        doExplosionRay(Vector2.right,  grid.cellSize.x * explosionRange);
+        doExplosionBranch(Vector2.up, grid.cellSize.y, explosionRange);
+        doExplosionBranch(Vector2.down, grid.cellSize.y, explosionRange);
+        doExplosionBranch(Vector2.left, grid.cellSize.x, explosionRange);
+        doExplosionBranch(Vector2.right,  grid.cellSize.x, explosionRange);
+        showExplosionEffect(transform.position);
     }
 
-    private void doExplosionRay(Vector2 direction, float distance)
+    private void doExplosionBranch(Vector2 direction, float stepSize, float explosionRange)
     {
+        float distance = stepSize * explosionRange;
+        Vector3 direction3 = new Vector3(direction.x, direction.y, 0f);
+        Vector3 stepInc = direction3 * stepSize;
+        Vector3 lastHit = transform.position + (direction3 * distance) + stepInc;
+
         RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction, distance);
-        Vector3 lastHit = transform.position + new Vector3(direction.x, direction.y, 0f) * distance;
         foreach (RaycastHit2D hit in hits)
         {
             string tag = hit.transform.gameObject.tag;
@@ -48,9 +54,22 @@ public class BombTimer : MonoBehaviour
             }
             else if ("ground".Equals(tag))
             {
-                lastHit = hit.transform.position;
+                distance = hit.distance;
                 break;
             }
         }
+
+        Vector3 destination = lastHit - stepInc;
+        Vector3 origin = transform.position + stepInc;
+        for (Vector3 step = origin; Vector3.Distance(step, origin) < distance; step += stepInc)
+        {
+            showExplosionEffect(step);
+        }
+    }
+
+    private void showExplosionEffect(Vector3 position)
+    {
+        GameObject particleEffect =  GameObject.Instantiate(explosionEffect);
+        particleEffect.transform.position = position;
     }
 }
